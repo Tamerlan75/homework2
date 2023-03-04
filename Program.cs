b064e69f548529793191e17430962e2ced380721
Timur <t.mollaev75@yandex.ru>
2023-03-04 21:20:19 +0300

    Задача1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int AverageDigit (int nam)
+{
+int dec = nam / 10;
+int average = dec%10;
+return average;
+}
+Console.WriteLine("Input three-digit namber");
+int nam = Convert.ToInt32(Console.ReadLine());
+int result = AverageDigit(nam);
+Console.WriteLine($"Average digit of {nam} -> {result}");
